2a8d816cb93e9f32011ad5578d66315726c9e4a2
AlissonSeraphim <projetoseraphim@gmail.com>
2023-03-15 01:08:03 -0300

    Requisito 4 OK

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -38,6 +39,9 @@ class Header extends React.Component {
             O nome é:
             { pushedName }
           </p>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
         </header>
       )
     );
